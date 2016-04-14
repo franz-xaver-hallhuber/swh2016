2d899f46c3e263f1553476df78b657510f12c786
nguyenma <mai-anh.nguyen@web.de>
2016-04-14 11:47:40 +0200

    PiezoTest

--- ArduinoCode/piezoTest/piezoTest.ino
+++ ArduinoCode/piezoTest/piezoTest.ino
@@ -1,30 +1,39 @@
-int redLED = 3;
-int piezoPin = A0;
-int threshold = 1000;
+const int blauHam =A0;
+//const int pinkHam = A2;
+const int ledPin= 4;
+const int threshold= 100;
 
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  pinMode(redLED, OUTPUT);
-
+void setup()
+{
+Serial.begin(9600);
   
+pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int val = analogRead(piezoPin);
-  Serial.println(val);
-  
-  if(val>=threshold) {
-    digitalWrite(redLED, HIGH);
-     delay(5000);
-  } else {
-    digitalWrite(redLED, LOW);
-  }
+void loop()
+{
+int valBlue = analogRead(blauHam);
+//int valPink = analogRead(pinkHam);
 
- 
-  
 
+if (valBlue <= threshold)
+{
+digitalWrite(ledPin, HIGH);
+delay(1000);
+Serial.print("Blue:");
+Serial.println(valBlue);
+digitalWrite(ledPin, LOW);
 }
+
+/*if (valPink>= threshold)
+{
+digitalWrite(ledPin, HIGH);
+delay(1000);
+Serial.print("Pink:");
+Serial.println(valPink);
+digitalWrite(ledPin, LOW);
+}*/
+
+else 
+digitalWrite(ledPin, LOW);
+} 
